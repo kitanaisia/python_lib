72764155f8b38b9c7d496095a97281bfa2500ee6
kitanaisia <kitanaisia@gmail.com>
2014-11-13 00:40:37 +0900

    Some comments are added for function "vital.dict_cosine"

--- vital.py
+++ vital.py
@@ -24,9 +24,14 @@ def file_read(file_path):
 def dict_cosine(dict1, dict2):
     import scipy.spatial.distance
 
+    # 基底となる単語の和集合を計算
     keys = set( dict1.keys() ) | set( dict2.keys() )
+
+    # 次元を揃えて，dictからコサイン類似度計算用のベクトルを生成
     vector1 = [ dict1.get(key, 0) for key in keys]
     vector2 = [ dict2.get(key, 0) for key in keys]
+
+    # cosine でコサイン距離を計算，1-cosineでコサイン類似度となる．
     sim = 1 - scipy.spatial.distance.cosine(vector1, vector2)
 
     return sim
